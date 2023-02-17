b530c7f799568237fe8fbe1f6c420b2a919c07d0
Aymen <AymenMir1001@gmail.com>
2023-02-17 16:07:58 +0100

    Update NftContract.sol

--- smart_contracts/contracts/NftContract.sol
+++ smart_contracts/contracts/NftContract.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.7;
+pragma solidity 0.8.10;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
